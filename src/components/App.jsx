128dd0d2718da3e6dae7929284d409faad71e6c9
andyseracuse <andrew.seracuse@gmail.com>
2020-09-25 16:58:24 -0600

    made carousel update with hardcoded style call without infinite re-render loop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
-import React, { useState, useLayoutEffect, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CarouselContainer from './CarouselContainer.jsx';
 
 function App(props) {
-  const [style, setStyle] = useState({});
-  useLayoutEffect(() => {
-    axios.get('http://52.26.193.201:3000/products/1/styles').then((response) => {
+  const [style, setStyle] = useState({ photos: [{ url: '', thumbnail: '' }] });
+  const getStyle = () => {
+    return axios.get('http://52.26.193.201:3000/products/1/styles').then((response) => {
       setStyle(response.data.results[1]);
     });
-  });
+  };
+
+  useEffect(() => {
+    getStyle();
+  }, []);
 
   return (
     <div className="container">
