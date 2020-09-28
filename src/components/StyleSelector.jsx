703acd412e1cc511a2987f774c64e6fb771c0446
andyseracuse <andrew.seracuse@gmail.com>
2020-09-28 11:35:28 -0600

    added a badge to the styles to indicate which style is selected

--- src/components/StyleSelector.jsx
+++ src/components/StyleSelector.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
+import Badge from '@material-ui/core/Badge';
 
 function StyleSelector({ styles, setStyleIndex, styleIndex }) {
+  const check = (
+    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+      <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+    </svg>
+  );
+
   return (
     <div className="row style-selector">
       {styles.map((style, index) => {
         return (
-          <div 
-            className={"ajs-style-btn rounded-circle col-3 " + (style.style_id === styles[styleIndex].style_id ? "ajs-style-btn" : "")} 
-            key={style.style_id} 
-            onClick={() => setStyleIndex(index)}
-          >
-            <Avatar src={style.photos[0].thumbnail_url} alt={style.name} />
+          <div key={style.style_id} className="col-3">
+            <Badge
+              badgeContent="✓"
+              color="primary"
+              invisible={style.style_id !== styles[styleIndex].style_id}
+            >
+              <Avatar
+                className="ajs-style-btn"
+                onClick={() => setStyleIndex(index)}
+                src={style.photos[0].thumbnail_url} alt={style.name} />
+            </Badge>
           </div>
         );
       })}
