1d69010a0a720444cfc02ed947d407596a475e33
andyseracuse <andrew.seracuse@gmail.com>
2020-09-28 16:20:46 -0600

    wrote tests for and implimented a review component

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Rating from '@material-ui/lab/Rating'
 
-function Review(props) {
+function Review({ ratings }) {
+  let total = 0;
+  let weightedTotal = 0;
+  for( const key in ratings) {
+    weightedTotal += Number(key)*ratings[key];
+    total += ratings[key];
+  }
+  const avgRating = weightedTotal / total;
+  const roundedAvg = (Math.round(avgRating * 4) / 4).toFixed(2);
+
   return (
-    <div>REVIEWS</div>
+    <div>
+      <Rating name="half-rating-read" defaultValue={Number(roundedAvg)} precision={0.5} readOnly />
+      <a href="#">Read all reviews</a>
+    </div>
   );
 }
 
