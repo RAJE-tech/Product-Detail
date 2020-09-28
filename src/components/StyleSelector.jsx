e216a2170f3d256965bdfa7c68507b91c8f113cb
andyseracuse <andrew.seracuse@gmail.com>
2020-09-28 14:23:47 -0600

    fixed styleing so that style btns are large

--- src/components/StyleSelector.jsx
+++ src/components/StyleSelector.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Badge from '@material-ui/core/Badge';
 
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
+
 function StyleSelector({ styles, setStyleIndex, styleIndex }) {
-  const check = (
-    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-      <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
-    </svg>
-  );
+  const classes = useStyles();
 
   return (
     <div className="row style-selector">
       {styles.map((style, index) => {
         return (
-          <div key={style.style_id} className="col-3">
+          <div key={style.style_id} className="col-sm-3 col-4 ajs-style-btn">
             <Badge
               badgeContent="✓"
               color="primary"
+              overlap="circle"
               invisible={style.style_id !== styles[styleIndex].style_id}
             >
               <Avatar
-                className="ajs-style-btn"
+                className={classes.large}
                 onClick={() => setStyleIndex(index)}
-                src={style.photos[0].thumbnail_url} alt={style.name} />
+                src={style.photos[0].thumbnail_url}
+                alt={style.name}
+              />
             </Badge>
           </div>
         );
