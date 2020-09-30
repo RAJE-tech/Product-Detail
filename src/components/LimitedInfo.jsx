ef3cfb4a69a8ef16f95e5f33e3b5572798ce45aa
andyseracuse <andrew.seracuse@gmail.com>
2020-09-29 18:13:52 -0600

    added functionality so that a sale price now appears in red next to a struck out original

--- src/components/LimitedInfo.jsx
+++ src/components/LimitedInfo.jsx
@@ -6,9 +6,12 @@ function LimitedInfo({ product, styles, styleIndex }) {
     <div>
       {product.category.toUpperCase()}
       <h2 className="ajs-product-name">Expanded Product Name</h2>
-      <p>
-        ${price}
-      </p>
+      <div className="ajs-prices">
+        {(styles[styleIndex].sale_price !== '0' ? <div className="ajs-struck">${styles[styleIndex].original_price}</div> : <div />)}
+        <p className={styles[styleIndex].sale_price !== '0' ? 'ajs-sale' : ''}>
+          ${price}
+        </p>
+      </div>
     </div>
   );
 }
