af8aad2d1afe0c7f442f8a0e84c30d71d087363b
andyseracuse <andrew.seracuse@gmail.com>
2020-10-05 20:23:26 -0600

    refactored multi carousel indicators to include custom arrows

--- src/components/CarouselIndicators.jsx
+++ src/components/CarouselIndicators.jsx
@@ -2,6 +2,27 @@ import React from 'react';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
+const CustomRightArrow = ({ onClick }) => {
+  // onMove means if dragging or swiping in progress.
+  return (
+    <button className="ajs-custom-right" onClick={() => onClick()}>
+      <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-chevron-right" fill="white" xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+      </svg>
+    </button>
+  );
+};
+const CustomLeftArrow = ({ onClick }) => {
+  // onMove means if dragging or swiping in progress.
+  return (
+    <button className="ajs-custom-left" onClick={() => onClick()}>
+      <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-chevron-left" fill="white" xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+      </svg>
+    </button>
+  );
+};
+
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
@@ -28,7 +49,12 @@ const responsive = {
 function CarouselIndicators({ items, activeIndex, onClickHandler }) {
   return (
     <div className="container ajs-indicator-container">
-      <Carousel containerClass={items.length < 7 ? 'react-mutli-carousel-list ajs-center-indicators' : 'react-mutli-carousel-list'} responsive={responsive}>
+      <Carousel
+        customRightArrow={<CustomRightArrow />}
+        customLeftArrow={<CustomLeftArrow />}
+        containerClass={items.length < 7 ? 'react-mutli-carousel-list ajs-center-indicators' : 'react-mutli-carousel-list'}
+        responsive={responsive}
+      >
         {items.map((item, index) => {
           return (
             <div
